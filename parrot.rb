dcad1759a2fea229d0fcfcd531f19f21d3a30396
malcolm staso <mstaso1@gmail.com>
2020-04-17 21:16:37 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,8 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot (name="squawk!")
-  puts "#{name}"
-  return "#{name}"
+  puts "{name}"
+  return "{name}"
 end
 
 
